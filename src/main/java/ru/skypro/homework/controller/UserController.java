70f09e09380859e4241adc27ee4af0aec842ff44
Anton <joongleboo01@gmail.com>
2024-04-08 17:04:04 +0300

    Создал DTO

---
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -0,0 +1,24 @@
+package ru.skypro.homework.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.NewPassword;
+
+@Slf4j
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/set_password")
+    public ResponseEntity<?> setPassword(@RequestBody NewPassword newPassword) {
+        return ResponseEntity.ok(new NewPassword());
+    }
+    //
+
+
+
+}
